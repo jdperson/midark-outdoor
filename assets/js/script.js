62d7e27d6de308ff432490b25f1f78d29ef9cfcb
JD <jondavidperson@gmail.com>
2024-04-02 20:33:03 -0500

    prototype build

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    const slides = document.querySelectorAll('.card');
+    if (n >= slides.length) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach((slide) => slide.classList.remove('active'));
+    slides[slideIndex].classList.add('active');
+}
